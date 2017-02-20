d59d1ba57d0d93f383f5f7f78e52ce532b7c0627
DESKTOP-A5VN6J1\bethd <bethdiary9955@gmail.com>
2017-02-20 23:21:23 +0900

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+//var stylus = require('gulp-stylus');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+
+var src = 'app/src/ball_bounce';
+var dist = 'app/dist';
+
+var paths = {
+    js: src + '/js/*.js',
+    styl: src + '/styl/*.styl',
+    html: src + '/**/*.html'
+};
+
+// gulp.task('js:combine', function () {
+//     return gulp.src(paths.js)
+//         .pipe(concat('combined.js'))
+//         .pipe(gulp.dest(dist+'/js'))
+//         //.pipe(uglify())
+//         .pipe(rename('combined.min.js'))
+//         .pipe(gulp.dest(dist+'/js')) });
+
+gulp.task('watch', function () {
+    gulp.watch(paths.js, ['babel']);
+    //gulp.watch(paths.scss, ['scss:compile']);
+});
+
+
+gulp.task('babel', function() {
+    return gulp.src(paths.js)
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(concat('all.js'))
+        //.pipe(gulp.dest(dist+'/js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(dist+'/js'))
+});
+gulp.task('default', ['babel','watch']);
+
+
+
